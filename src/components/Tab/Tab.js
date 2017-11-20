1f78e2cf811afdb8f85fdd6ed82cc84aa1c03805
fxf111 <fxf12345@139.com>
2017-11-20 09:43:30 +0800

    底部Tab导航栏-公用组件开发（进行中）

--- src/components/Tab/Tab.js
+++ src/components/Tab/Tab.js
@@ -1,10 +1,25 @@
-import React,{Component} from 'react';
-export default class Tab extends Component{
-    render(){
+import React, {Component} from 'react';
+import {Link, NavLink} from 'react-router-dom';
+import './Tab.less'
+
+export default class Tab extends Component {
+    render() {
         return (
-            <div>
-                tab
-            </div>
+            <ul className='footerTab'>
+                <Link to='/'><img src="http://p12.jmstatic.com/mcms/5e9e00537e4a635e36e84db81f2cc17a.png" alt=""/>
+                    <span>首页</span></Link>
+                <Link to='/yiqituan'><img src="http://p12.jmstatic.com/mcms/b51a66ca3ddb31ec0edbd67ea68f7589.png"
+                                          alt=""/>
+                    <span>拼团</span>
+                </Link>
+                <Link to='/cart'><img src="http://p12.jmstatic.com/mcms/77b9454e8dd41a060510a445010ac934.png" alt=""/>
+                    <span>购物车</span>
+                </Link>
+                <Link to='/profile'><img src="http://p12.jmstatic.com/mcms/829aa091062366a9f7b93cfffaa0e681.png"
+                                         alt=""/>
+                    <span>个人</span>
+                </Link>
+            </ul>
         )
     }
 }
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,12 +1,26 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
+import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.less';
 import Tab from '../../components/Tab/Tab';
-export default class App extends Component{
-    render(){
+import Home from "../Home/Home";
+import Demo from "../Demo/Demo";
+import Cart from "../Cart/Cart";
+import Yiqituan from "../Yiqituan/Yiqituan";
+import Profile from "../Profile/Profile";
+
+export default class App extends Component {
+    render() {
         return (
-            <div className='app'>
-                <Tab/>
-            </div>
+            <Router>
+                <div className='app'>
+                    <Tab/>
+                    <Route exact path='/' component={Home}/>
+                    <Route path='/demo' component={Demo}/>
+                    <Route path='/cart' component={Cart}/>
+                    <Route path='/yiqituan' component={Yiqituan}/>
+                    <Route path='/profile' component={Profile}/>
+                </div>
+            </Router>
         )
     }
 }
