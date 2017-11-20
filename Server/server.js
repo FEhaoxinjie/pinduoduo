6a21f0c6d407ea9ae0fdd94c9a3fa08cc1e2898e
fxf111 <fxf12345@139.com>
2017-11-20 12:59:39 +0800

    给window绑定onresize事件，当窗口大小发生改变时，自动计算根字体大小

--- Server/server.js
+++ Server/server.js
@@ -1,3 +1,37 @@
 let express = require('express');
-let fs=require('fs');
-let app=express();
+let fs = require('fs');
+let app = express();
+
+function getUserList(cb) {
+    fs.readFile('./mock/user.json', 'utf-8', (err, data) => {
+        cb(data);
+    })
+}
+
+function addUser(user, cb) {
+    fs.readFile('./mock/user.json', 'utf-8', (err, data) => {
+        let userList = JSON.parse(data);
+        let isReg = userList.some(item => {
+            return item.tel == user.tel
+        });
+        if (isReg) {
+            cb({code: 100, error: '该手机号已经被注册!'})
+        } else {
+            userList.push({
+                ...user,
+                userName: user.tel
+            });
+            fs.writeFile('./mock/user.json', userList, function (err) {
+                if (err) {
+                    cb({
+                        code: 100,
+                        error: '服务器出错'
+                    })
+                } else {
+
+                }
+            })
+
+        }
+    })
+}
